8f777ba5c008ba5fc60de144041876bbc673857d
Ezekiel Oshin <ezekieloshin@gmail.com>
2023-11-14 18:02:45 +0100

    updating iteration

--- iterators.py
+++ iterators.py
@@ -20,10 +20,48 @@ print(next(myit))
 print(next(myit))
 print(next(myit))
 
+#Create an iterator that returns numbers,
+# starting with 1, and each sequence will increase by one (returning 1,2,3,4,5 etc.):
 
+class MyNumbers:
+  def __iter__(self):
+    self.a = 1
+    return self
 
+  def __next__(self):
+    x = self.a
+    self.a += 1
+    return x
 
+myclass = MyNumbers()
+myiter = iter(myclass)
 
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+
+#Stop after 20 iterations:
+
+class MyNumbers:
+  def __iter__(self):
+    self.a = 1
+    return self
+
+  def __next__(self):
+    if self.a <= 20:
+      x = self.a
+      self.a += 1
+      return x
+    else:
+      raise StopIteration
+
+myclass = MyNumbers()
+myiter = iter(myclass)
+
+for x in myiter:
+  print(x)
 
 
 
