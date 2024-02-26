d8d9a9b10dd91874c7d52b4fb5a37ca607e4ba55
*Ezekiel <*ezekieloshin@gmail.com*>
2024-02-26 21:37:19 +0100

    adding gender impact on nobel winners to the learn python project

--- gender_impact.py
+++
@@ -1,9 +0,0 @@
-# Importing the relevant python libraries for our analysis
-import pandas as pd
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-# Loading the dataset
-nobel = pd.read_csv(r"C:\Users\Podos\OneDrive\Desktop\Data science\Dataset\Nobel\nobel.csv")
-nobel.head()
